0a8b7279dfff5fdeb25a5e1baf6645ee6ad7f9ac
TICHOJEBEC <150498032+TICHOJEBEC-SK@users.noreply.github.com>
2024-09-19 19:53:38 +0200

    Add files via upload

---
+++ Penis.cs
@@ -0,0 +1,77 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Modules.Commands;
+using CounterStrikeSharp.API.Modules.Utils;
+
+namespace Penis
+{
+    public partial class Penis : BasePlugin
+    {
+        private readonly Dictionary<CCSPlayerController, DateTime> playerLastCommandTime = new Dictionary<CCSPlayerController, DateTime>();
+
+        public override string ModuleAuthor => "TICHOJEBEC";
+        public override string ModuleName => "Penis";
+        public override string ModuleVersion => "v1.0";
+
+        private double GeneratePenisSize(string playerName)
+        {
+            int seed = playerName.GetHashCode();
+            Random random = new Random(seed);
+            return (random.Next(1, 51) + random.NextDouble());
+        }
+
+        [ConsoleCommand("css_cicina", "Odmerá tvoju cicinu")]
+        public void OnPenisCommand(CCSPlayerController? player, CommandInfo command)
+        {
+            if (player == null || !player.IsValid || player.IsBot || player.IsHLTV)
+            {
+                return;
+            }
+
+            if (playerLastCommandTime.TryGetValue(player, out DateTime lastCommandTime))
+            {
+                var elapsedTime = (DateTime.Now - lastCommandTime).TotalSeconds;
+                if (elapsedTime < 120)
+                {
+                    var remainingTime = 120 - elapsedTime;
+                    player.PrintToChat($" {ChatColors.Red}𝗖𝗦𝗞𝗢.𝗡𝗘𝗧 ● {ChatColors.Default}Tento príkaz môžeš použiť iba raz za {ChatColors.Red}{remainingTime:F0} {ChatColors.Default}sekúnd.");
+                    return;
+                }
+            }
+
+            Random random = new Random();
+            double penisSize = random.Next(1, 51) + random.NextDouble();
+            string formattedPenisSize = penisSize.ToString("0.00");
+
+            Server.PrintToChatAll($" {ChatColors.Red}𝗖𝗦𝗞𝗢.𝗡𝗘𝗧 ● {ChatColors.Default}Hráč {ChatColors.Green}{player.PlayerName}{ChatColors.Default} má {ChatColors.Green}{formattedPenisSize} {ChatColors.Default}centimetrovú cicinu.");
+            playerLastCommandTime[player] = DateTime.Now;
+        }
+
+        [ConsoleCommand("css_realcicina", "Odmerá tvoju cicinu na základe mena")]
+        public void OnRealPenisCommand(CCSPlayerController? player, CommandInfo command)
+        {
+            if (player == null || !player.IsValid || player.IsBot || player.IsHLTV)
+            {
+                return;
+            }
+
+            if (playerLastCommandTime.TryGetValue(player, out DateTime lastCommandTime))
+            {
+                var elapsedTime = (DateTime.Now - lastCommandTime).TotalSeconds;
+                if (elapsedTime < 120)
+                {
+                    var remainingTime = 120 - elapsedTime;
+                    player.PrintToChat($" {ChatColors.Red}𝗖𝗦𝗞𝗢.𝗡𝗘𝗧 ● {ChatColors.Default}Tento príkaz môžeš použiť iba raz za {ChatColors.Red}{remainingTime:F0} {ChatColors.Default}sekúnd.");
+                    return;
+                }
+            }
+
+            double penisSize = GeneratePenisSize(player.PlayerName);
+            string formattedPenisSize = penisSize.ToString("0.00");
+
+            Server.PrintToChatAll($" {ChatColors.Red}𝗖𝗦𝗞𝗢.𝗡𝗘𝗧 ● {ChatColors.Default}Hráč {ChatColors.Green}{player.PlayerName}{ChatColors.Default} má reálnu {ChatColors.Green}{formattedPenisSize} {ChatColors.Default}centimetrovú cicinu.");
+            playerLastCommandTime[player] = DateTime.Now;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# cs2-Penis
+<h1 align="center">
+  CS2 Penis
+</h1>
+
+<p align="center">
+<i>Loved the tool? Please consider <a href="https://paypal.com/paypalme/cskonet">donating</a>  💸 to help it improve!</i>
+</p>
+
+<p align="center">
+<a href="https://www.paypal.com/paypalme/cskonet"><img src="https://img.shields.io/badge/support-PayPal-blue?logo=PayPal&style=flat-square&label=Donate"/>
+</a>
+
+#### ABOUT THE PLUGIN
+
+This fun plugin adds two commands that allow players to measure their "penis," with the results displayed in the chat. The first use of something like this was by the well-known Slovak streamer [2SekundovyMato](https://www.youtube.com/@2SekundovyMato), who inspired this plugin.
+
+**Plugin functionality:**
+
+1. **Command `css_cicina`**:
+   - This command, when entered in the game, randomly generates a penis size for the player.
+   - If the player has used the command in the last 120 seconds, the command will not execute and the player will receive a message about the remaining time until they can use it again.
+   - The randomly generated size (from 1.00 to 50.99 cm) is then displayed to all players on the server.
+
+2. **Command `css_realcicina`**:
+   - This command creates a penis size based on the player’s name rather than generating it randomly.
+   - It uses a hash function (`playerName.GetHashCode()`) to generate a consistent penis size for each player based on their name.
+   - Similarly to `css_cicina`, if the command has been used in the last 120 seconds, the player must wait before they can use the command again.
+   - The penis size is displayed in the chat to all players as the "real" size.
+
+This plugin allows measuring the "penis" either randomly or based on a consistent calculation from the player’s name, with both commands having a usage time limit.
+
+## 💡 Contact
+**If you are interested in this plugin, contact me via private message.**
+
+Discord: tichotm
+
+## 🛠️ Installation Steps
+
+**Required for functionality plugin**
+- [CounterStrikeSharp](https://github.com/roflmuffin/CounterStrikeSharp) 
+
+**How to install?**
+1. Begin by downloading the contents and then proceed to open the downloaded zip file.
+2. Within the extracted files, locate the 'Gameserver' folder.
+3. Drag all the files found within the 'Gameserver' folder and drop them into the '/game/csgo/addons/counterstrikesharp/plugins' directory on your game server.
+
+## 🙇 Sponsors
+- Be the first to support this project!
+
+## 🙏 Support
+<p align="left">
+<a href="https://paypal.com/paypalme/cskonet"><img src="https://ionicabizau.github.io/badges/paypal.svg">
+</a>
+</p>
